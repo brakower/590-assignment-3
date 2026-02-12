730603cb4bdf38edc0b5ae09e894c2dcc9a52b9c
bennyrak <brakower1@unc.edu>
2026-02-11 21:16:15 -0500

    First part of assignment

---
+++ app.erl
@@ -0,0 +1,33 @@
+-module(app).
+-export([start/0]).
+
+start() ->
+    io:format("Enter a number: "),
+    Input = io:get_line(""),
+    Str = string:trim(Input),
+    case catch list_to_integer(Str) of
+        {'EXIT', _} ->
+            io:format("not an integer~n");
+        N when is_integer(N) ->
+            handle_number(N)
+    end.
+
+handle_number(N) when N < 0 ->
+    Result = trunc(math:pow(abs(N), 7)),
+    io:format("~p~n", [Result]);
+
+handle_number(0) ->
+    io:format("0~n");
+
+handle_number(N) when N > 0 ->
+    case N rem 7 of
+        0 ->
+            Result = math:pow(N, 1/5),
+            io:format("~p~n", [Result]);
+        _ ->
+            Result = factorial(N),
+            io:format("~p~n", [Result])
+    end.
+
+factorial(0) -> 1;
+factorial(N) -> N * factorial(N - 1).
